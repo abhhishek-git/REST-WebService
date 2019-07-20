c07d53efc5376560755c3b9b2826dc7b215af810
Abhishek Singh <abhiwanttochat@gmail.com>
2019-07-20 17:08:27 +0530

    annotations for operations and path-  JAXRS annotations

--- restws/src/main/java/com/bharaththippireddy/trainings/jaxrs/PatientService.java
+++ restws/src/main/java/com/bharaththippireddy/trainings/jaxrs/PatientService.java
@@ -1,16 +1,30 @@
 package com.bharaththippireddy.trainings.jaxrs;
 
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
 public interface PatientService {
-
-	Patient getPatient(String id);
-
+	
+	@GET
+	@Path("/patients/{id}/")
+	Patient getPatient(@PathParam("id") String id);
+	
+	@PUT
+	@Path("/patients/")
 	Response updatePatient(Patient patient);
-
+	
+	@POST
+	@Path("/patients/")
 	Response addPatient(Patient patient);
-
-	Response deletePatients(String id);
+	
+	@DELETE
+	@Path("/patients/{id}")
+	Response deletePatients(@PathParam("id") String id);
 
 	Prescription getPrescription(String prescriptionId);
 
